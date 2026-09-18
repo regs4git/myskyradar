2463419e16fcb0344d9bdb38feefa598fb345cef
regs4git <mywwwregs@gmail.com>
2026-09-18 11:15:39 +0100

    Increment cache version to v1.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VERSÃO DA CACHE - Incrementar sempre que houver alterações
-const CACHE_VERSION = 'v1.1';
+const CACHE_VERSION = 'v1.2';
 const CACHE_NAME = `myskyradar-${CACHE_VERSION}`;
 
 const ASSETS = [
